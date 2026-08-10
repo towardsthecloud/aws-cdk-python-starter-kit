101281763c5e450f60da573688fd5e1316231ceb
Axon Stone <axonstone@towardsthecloud.com>
2026-08-11 00:26:03 +0200

    feat: validate CDK apps before deployment

--- src/bin/env_helper.py
+++ src/bin/env_helper.py
@@ -1,25 +1,26 @@
-from typing import Dict
-
 from projen.awscdk import AwsCdkPythonApp
 
 
-def cdk_action_task(project: AwsCdkPythonApp, target_account: Dict[str, str]):
-    task_actions = ["synth", "diff", "deploy", "destroy"]
+def cdk_action_task(project: AwsCdkPythonApp, target_account: dict[str, str]):
     stack_name_pattern = f"*Stack-{target_account['ENVIRONMENT']}"
+    action_commands = {
+        "synth": ("cdk synth", False),
+        "validate": ("cdk --unstable=validate validate", True),
+        "diff": (f"cdk diff --require-approval never {stack_name_pattern}", False),
+        "deploy": (f"cdk deploy --require-approval never {stack_name_pattern}", False),
+        "destroy": (f"cdk destroy --force {stack_name_pattern}", False),
+    }
 
-    for action in task_actions:
+    for action, (exec_command, receive_args) in action_commands.items():
         task_name = f"{target_account['ENVIRONMENT']}:{action}"
         task_description = f"{action.capitalize()} the stacks on the {target_account['ENVIRONMENT'].upper()} account"
 
-        exec_command = f"cdk {action} --require-approval never {stack_name_pattern}"
-        if action == "destroy":
-            exec_command = f"cdk destroy --force {stack_name_pattern}"
-        if action == "synth":
-            exec_command = "cdk synth"
-
-        project.add_task(
+        task = project.add_task(
             task_name,
             description=task_description,
             env=target_account,
-            exec=exec_command,
         )
+        if receive_args:
+            task.exec(exec_command, receive_args=True)
+        else:
+            task.exec(exec_command)
